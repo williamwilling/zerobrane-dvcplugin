f3f6a2b69cc1b6ce2578c058b06f2a54b966e203
Joost Ronkes Agerbeek <raj@davinci.nl>
2014-08-26 13:02:37 +0200

    Interpreters that aren't whitelisted will be removed from the Projects\Lua Interpreters menu.

--- src/davinci.lua
+++ src/davinci.lua
@@ -1,3 +1,16 @@
+local function removeInterpreters()
+  local whitelist = {
+    luadeb = true,
+    love2d = true
+  }
+  
+  for interpreter in pairs(ide.interpreters) do
+    if not whitelist[interpreter] then
+      ide:RemoveInterpreter(interpreter)
+    end    
+  end
+end
+
 return {
   name = "Da Vinci Environment plugin",
   description = "Sets up ZeroBrane Studio for use in the classes taught at the Da Vinci College in Dordrecht, The Netherlands.",
@@ -5,5 +18,6 @@ return {
   version = 1,
   
   onRegister = function (self)
+    removeInterpreters()
   end
 }
