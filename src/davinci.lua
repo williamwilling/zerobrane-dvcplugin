a1ce047e3dab0e3bb380acd49b689e1ce990fb77
Joost Ronkes Agerbeek <raj@davinci.nl>
2014-08-26 12:01:25 +0200

    Added basic code for the plugin.

---
+++ src/davinci.lua
@@ -0,0 +1,9 @@
+return {
+  name = "Da Vinci Environment plugin",
+  description = "Sets up ZeroBrane Studio for use in the classes taught at the Da Vinci College in Dordrecht, The Netherlands.",
+  author = "William Willing",
+  version = 1,
+  
+  onRegister = function (self)
+  end
+}
