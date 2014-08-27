acab6b6124c2036b0a458e4280116881ec2c416f
Joost Ronkes Agerbeek <raj@davinci.nl>
2014-08-27 12:14:03 +0200

    The text ZeroBrane adds to your program output is now disabled.

--- src/davinci.lua
+++ src/davinci.lua
@@ -11,6 +11,25 @@ local function removeInterpreters()
   end
 end
 
+local function disableOutputText()
+  local _displayOutputLn = DisplayOutputLn
+  DisplayOutputLn = function(str, ...)
+    if str ~= "" then
+      _displayOutputLn(str, ...)
+    end
+  end
+
+  local _tr = TR
+  TR = function(str)
+    local altered = {
+      ["Program starting as '%s'."] = "",
+      ["Program '%s' started in '%s' (pid: %d)."] = ""
+    }
+
+    return altered[str] or _tr(str)
+  end
+end
+
 return {
   name = "Da Vinci Environment plugin",
   description = "Sets up ZeroBrane Studio for use in the classes taught at the Da Vinci College in Dordrecht, The Netherlands.",
@@ -18,6 +37,7 @@ return {
   version = 1,
   
   onRegister = function (self)
-    removeInterpreters()
+    removeInterpreters()    -- Only show the interpreters we actually use.
+    disableOutputText()     -- When you run your program, you should see only the text your program generates and not some preamble.
   end
 }
